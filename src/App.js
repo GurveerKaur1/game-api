e01096ac03472be12340aebf7a17e742cc282eb0
Gurveer <gurveer2202@gmail.com>
2023-06-16 15:02:41 -0500

    add changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Catalog from './components/Catalog';
+import './style/index.css';
+import Details from './components/Details';
+import Header from './components/Header';
+import New from './components/New';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Header></Header>
+     <Routes>
+      <Route path = "/game-api" element={<Catalog/>}></Route>
+      <Route path="/product/:selectedProduct" element={<Details />} />
+      <Route path="/new" element={<New/>}></Route>
+     </Routes>
+     <Footer/>
     </div>
   );
 }
